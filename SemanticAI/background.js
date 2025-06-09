5070cdf89b015f81a90ba576bcb85447eff4a6b0
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-06-09 16:37:08 +0100

    added a backend for the gemini api call

--- SemanticAI/background.js
+++ SemanticAI/background.js
@@ -1,3 +1,32 @@
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	console.log("Extension installed");
+
+	if (message.action === "callGeminiAPI") {
+		handleGeminiAPI(message.prompt, message.images, sendResponse);
+	}
 });
+
+const handleGeminiAPI = async (prompt, imageUrls, sendResponse) => {
+	try {
+		const res = await fetch("http://localhost:3000/gemini-api", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				prompt,
+			}),
+		});
+
+		if (!res.ok) {
+			console.error("Gemini API error:", await res.text());
+			sendResponse({ error: "API call failed" });
+			return;
+		}
+		const data = await res.json();
+		console.log("Gemini response:", data);
+	} catch (err) {
+		console.error("Error calling Gemini API:", err);
+		sendResponse({ error: err.message });
+	}
+};
--- SemanticAI/content.js
+++ SemanticAI/content.js
@@ -2,17 +2,27 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	console.log("Received in content script:", message);
 
 	const { heading, paragraph } = message;
-	const h1 = document.querySelector("h1");
-	const p = document.querySelector("p");
-	if (h1) {
-		h1.textContent = heading;
-	} else {
-		console.log("No <h1> found on this page");
-	}
 
-	if (p) {
-		p.textContent = paragraph;
-	} else {
-		console.log("No <p> found on this page");
-	}
+	const noAltImages = [];
+
+	const images = document.querySelectorAll("img");
+	images.forEach((image) => {
+		if (image.alt === "") {
+			console.log("No alt");
+			noAltImages.push(image.src);
+		} else {
+			console.log("There is alt");
+		}
+	});
+
+	if (noAltImages.length === 0) return;
+	const prompt = `Generate descriptive alt text for the following image URLS: \n\n${noAltImages
+		.map((url) => `-${url}`)
+		.join("\n")}`;
+
+	chrome.runtime.sendMessage({
+		action: "callGeminiAPI",
+		prompt: prompt,
+		images: noAltImages,
+	});
 });
